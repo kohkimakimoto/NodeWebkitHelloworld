d98067835ae3eff1f5ed809bc4e8b58336e87d35
kohkimakimoto <kohki.makimoto@gmail.com>
2014-12-16 16:18:40 +0900

    up

--- src/index.events.js
+++ src/index.events.js
@@ -1,3 +1,18 @@
+"use strict";
+
+(function() {
+    if (process.platform == 'darwin') {
+        var gui = require('nw.gui');
+        var mb = new gui.Menu({
+            type: "menubar"
+        });
+        var win = gui.Window.get();
+
+        mb.createMacBuiltin(require("../package.json").name);
+        win.menu = mb;
+    }
+})();
+
 $(document).ready(function(){
   $('#mybutton').click( function() {
     $('#mylabel').text('Hello World');
