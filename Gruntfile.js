bc2e2704b59ad0b182eaa4c131150832dd352f43
kohkimakimoto <kohki.makimoto@gmail.com>
2014-12-26 12:44:46 +0900

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,20 @@
+"use strict";
+
 module.exports = function(grunt) {
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Configurations for grunt
     grunt.initConfig({
-      nodewebkit: {
-        options: {
-            platforms: ['win','osx'],
-            buildDir: 'build',
+        nodewebkit: {
+            options: {
+                platforms: ['win', 'osx'],
+                buildDir: 'build',
+            },
+            src: ['package.json', 'src/**'],
         },
-        src: ['package.json', 'src/**'],
-      },
     });
 
-    grunt.loadNpmTasks('grunt-node-webkit-builder');
     grunt.registerTask('build', ['nodewebkit']);
-}
+};
